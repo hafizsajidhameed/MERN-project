caf24c6d513fd5d4d3f983c9b1b9cb5ac5d0ca9c
Sajid Hameed <sajid.hameed@sixlogics.local>
2024-10-06 21:53:47 +0200

    'changes'

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MERN-project/', // Set this to the repo name for GitHub Pages
   plugins: [react()],
   server: {
-    proxy:{
+    proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
       }
     }
   }
-})
+});
+
